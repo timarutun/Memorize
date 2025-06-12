0166d37d40c3a1af032f9400bbeafbd5499d97d9
timarutun <arutiunian.timur@yahoo.com>
2025-06-11 23:54:06 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Timur on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
